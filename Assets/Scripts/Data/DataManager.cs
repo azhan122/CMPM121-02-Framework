47c88bcc347b2d54bd393225763e319018d93103
chimpin <cbonardi@ucsc.edu>
2026-04-25 13:14:37 -0700

    Enemy Dictionary
    Added enemy lookup by name to DataManager for more readable code

--- Assets/Scripts/Data/DataManager.cs
+++ Assets/Scripts/Data/DataManager.cs
@@ -7,12 +7,12 @@ using System.Collections.Generic;
 // seperates gameplay logic from data handling for EnemySpawner 
 public class DataManager : MonoBehaviour
 {
-    // stores data from levels.json
+    // stores data from levels.json and enemies.js
     public List<JObject> levelData;
-
-    // stores data from enemies.json
     public List<JObject> enemyData;
 
+    // faster lookup of enemies using names
+     public Dictionary<string, JObject> enemyMap;
 
      // called before start on all objects so data is ready
     void Awake()
@@ -30,5 +30,13 @@ public class DataManager : MonoBehaviour
         // parse level and enemy definitions
         levelData = JsonConvert.DeserializeObject<List<JObject>>(levelsFile.text);
         enemyData = JsonConvert.DeserializeObject<List<JObject>>(enemiesFile.text);
+
+        // create dictionary from loaded enemies
+        enemyMap = new Dictionary<string, JObject>();
+        foreach (var enemy in enemyData)
+        {
+            string name = enemy["name"].ToString();
+            enemyMap[name] = enemy;
+        }
     }
 }
