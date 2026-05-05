9cbb5984876d672e44191a3a032f5cd7619435fd
chimpin <cbonardi@ucsc.edu>
2026-05-04 21:04:20 -0700

    Load spells.json
    DataManager loads spells just like enemies and levels

--- Assets/Scripts/Data/DataManager.cs
+++ Assets/Scripts/Data/DataManager.cs
@@ -14,6 +14,10 @@ public class DataManager : MonoBehaviour
     // faster lookup of enemies using names
      public Dictionary<string, JObject> enemyMap;
 
+    // stores data from spells.json and makes faster lookup
+     public List<JObject> spellData;
+    public Dictionary<string, JObject> spellMap;
+
      // called before start on all objects so data is ready
     void Awake()
     {
@@ -23,13 +27,15 @@ public class DataManager : MonoBehaviour
      // reads json files and parses them
     void LoadData()
     {
-        // load level and enemy definitions
+        // load level enemy and spell definitions
         TextAsset levelsFile = Resources.Load<TextAsset>("levels");
         TextAsset enemiesFile = Resources.Load<TextAsset>("enemies");
+        TextAsset spellsFile = Resources.Load<TextAsset>("spells");
 
-        // parse level and enemy definitions
+        // parse level enemy and spell definitions
         levelData = JsonConvert.DeserializeObject<List<JObject>>(levelsFile.text);
         enemyData = JsonConvert.DeserializeObject<List<JObject>>(enemiesFile.text);
+        spellData = JsonConvert.DeserializeObject<List<JObject>>(spellsFile.text);
 
         // create dictionary from loaded enemies
         enemyMap = new Dictionary<string, JObject>();
