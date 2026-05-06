a9dba29fd4e8bd7a17c57e7329204d0a038f4015
chimpin <cbonardi@ucsc.edu>
2026-05-05 18:20:28 -0700

    SpellBuilder skeleton
    Created skeleton code for SpellBuilder within Spell Build

--- Assets/Scripts/Spells/SpellBuilder.cs
+++ Assets/Scripts/Spells/SpellBuilder.cs
@@ -9,9 +9,27 @@ public class SpellBuilder
 {
 
     public Spell Build(SpellCaster owner)
+{
+    DataManager dm = GameObject.FindFirstObjectByType<DataManager>();
+
+    // collect base spells
+
+    foreach (var spell in dm.spellData)
     {
-        return new Spell(owner);
+        // base spells damage
+        if (spell["damage"] != null)
+        {
+            baseSpells.Add(spell);
+        }
     }
+
+    // pick a random base spell
+
+    // create Spell and inject JSON
+    Spell newSpell = something;
+
+    return newSpell;
+}
 
    
     public SpellBuilder()
