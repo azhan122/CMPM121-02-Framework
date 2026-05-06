43ccfb1c75afff573aeef3f49e5331e690ba33e9
chimpin <cbonardi@ucsc.edu>
2026-05-05 18:24:42 -0700

    SpellBuilder updated
    SpellBuilder constructs random base spells from spells.json using DataManager

--- Assets/Scripts/Spells/SpellBuilder.cs
+++ Assets/Scripts/Spells/SpellBuilder.cs
@@ -9,27 +9,38 @@ public class SpellBuilder
 {
 
     public Spell Build(SpellCaster owner)
-{
-    DataManager dm = GameObject.FindFirstObjectByType<DataManager>();
-
-    // collect base spells
-
-    foreach (var spell in dm.spellData)
     {
-        // base spells damage
-        if (spell["damage"] != null)
+        DataManager dm = GameObject.FindFirstObjectByType<DataManager>();
+
+        // collect base spells 
+        List<JObject> baseSpells = new List<JObject>();
+
+        foreach (var spell in dm.spellData)
         {
-            baseSpells.Add(spell);
+            // base spells damage
+            if (spell["damage"] != null)
+            {
+                baseSpells.Add(spell);
+            }
         }
+
+        // safety check
+        if (baseSpells.Count == 0)
+        {
+            Debug.LogError("No base spells found!");
+            return new Spell(owner);
+        }
+
+        // pick a random base spell
+        int index = Random.Range(0, baseSpells.Count);
+        JObject chosen = baseSpells[index];
+
+        // create Spell and inject json
+        Spell newSpell = new Spell(owner);
+        newSpell.SetAttributes(chosen);
+
+        return newSpell;
     }
-
-    // pick a random base spell
-
-    // create Spell and inject JSON
-    Spell newSpell = something;
-
-    return newSpell;
-}
 
    
     public SpellBuilder()
